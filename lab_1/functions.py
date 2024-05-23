cbd3a0523e1df8947e9ffea48977da9be4972090
afrodetka <Pro100biv@mail.ru>
2024-05-23 17:39:06 +0400

    Created key, caesar_encryption, input/ouput files and paths.py

--- lab_1/functions.py
+++ lab_1/functions.py
@@ -1,7 +1,7 @@
 import json
 from constants import ALPHABET
 
-def read_file(file_path):
+def read_file(file_path: str) -> str:
     """
     Read the file.
     Args:
@@ -17,22 +17,22 @@ def read_file(file_path):
     except Exception as e:
         raise e
 
-def write_file(file_path, content):
+def write_file(file_path: str, file_content: str) -> None:
     """
     Write to file.
     Args:
     - file_path (str): The path to the file to be written.
     - content (str): The content to write to the file.
     Returns:
-        None
+    - None
     """
     try:
         with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(content)
+            file.write(file_content)
     except Exception as e:
         raise e
 
-def read_json(file_path):
+def read_json(file_path: str) -> dict:
     """
     Read JSON from file.
     Args:
@@ -47,3 +47,24 @@ def read_json(file_path):
         raise FileNotFoundError(f"The file {file_path} does not exist") from e
     except Exception as e:
         raise e
+    
+def caesar_encryption(file_text: str, file_key: str) -> str:
+    """
+    Encrypt text using the Caesar cipher.
+    Args:
+    - file_text (str): The text to be encrypted.
+    - file_key (str): The key value for the Caesar cipher.
+    Returns:
+    - str: The encrypted text.
+    """
+    key = read_json(file_key)
+    text = read_file(file_text)
+    encrypted_result = ''
+    for char in text:
+            if char in ALPHABET:
+                encrypted_result += key[char]
+            else:
+                encrypted_result += char
+    return encrypted_result
+
+    
