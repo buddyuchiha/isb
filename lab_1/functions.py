d03690a4b2f359d30249f6dc9ef7adf073c80a72
afrodetka <Pro100biv@mail.ru>
2024-05-23 16:00:01 +0400

    Realization the function of reading, writing. Added constants and docstings.

--- lab_1/functions.py
+++ lab_1/functions.py
@@ -1,17 +1,49 @@
 import json
+from constants import ALPHABET
 
 def read_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+    """
+    Read the file.
+    Args:
+    - file_path (str): The path to the file to be read.
+    Returns:
+    - str: The content of the file.
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"The file {file_path} does not exist") from e
+    except Exception as e:
+        raise e
 
 def write_file(file_path, content):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(content)
+    """
+    Write to file.
+    Args:
+    - file_path (str): The path to the file to be written.
+    - content (str): The content to write to the file.
+    Returns:
+        None
+    """
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+    except Exception as e:
+        raise e
 
 def read_json(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
-
-def write_json(file_path, data):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4)
+    """
+    Read JSON from file.
+    Args:
+    - file_path (str): The path to the JSON file to be read.
+    Returns:
+    - dict: The JSON data.
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"The file {file_path} does not exist") from e
+    except Exception as e:
+        raise e
